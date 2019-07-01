16abe13a65cb833bb9150989044b7ef4abff96b2
Jakob Krigovsky <jakob@krigovsky.com>
2019-07-01 20:19:05 +0200

    Use NAN’s helper method for GetFunction()

--- icuWrapper.cpp
+++ icuWrapper.cpp
@@ -72,7 +72,7 @@ NAN_METHOD(DetectCharacterEncoding) {
 
 void Init(v8::Local<v8::Object> exports) {
 	exports->Set(Nan::New<v8::String>("detectCharacterEncoding").ToLocalChecked(),
-		Nan::New<v8::FunctionTemplate>(DetectCharacterEncoding)->GetFunction());
+		Nan::GetFunction(Nan::New<v8::FunctionTemplate>(DetectCharacterEncoding)).ToLocalChecked());
 }
 
 NODE_MODULE(icuWrapper, Init);
